0db6062a59ef8454f0747c4d65e029b368e0d92b
aiden <133913240+ywxweal@users.noreply.github.com>
2024-12-20 04:29:57 -0600

    Create GuidedRefTargetMonoTransform.cs

---
+++ GuidedRefTargetMonoTransform.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace GorillaTag.GuidedRefs
+{
+	public class GuidedRefTargetMonoTransform : MonoBehaviour, IGuidedRefTargetMono, IGuidedRefMonoBehaviour, IGuidedRefObject
+	{
+		[SerializeField]
+		private GuidedRefBasicTargetInfo guidedRefTargetInfo;
+
+		GuidedRefBasicTargetInfo IGuidedRefTargetMono.GRefTargetInfo
+		{
+			get
+			{
+				return guidedRefTargetInfo;
+			}
+			set
+			{
+				guidedRefTargetInfo = value;
+			}
+		}
+
+		public Object GuidedRefTargetObject => base.transform;
+
+		protected void Awake()
+		{
+			((IGuidedRefObject)this).GuidedRefInitialize();
+		}
+
+		protected void OnDestroy()
+		{
+			GuidedRefHub.UnregisterTarget(this);
+		}
+
+		void IGuidedRefObject.GuidedRefInitialize()
+		{
+			GuidedRefHub.RegisterTarget(this, guidedRefTargetInfo.hubIds, this);
+		}
+
+		int IGuidedRefObject.GetInstanceID()
+		{
+			return GetInstanceID();
+		}
+	}
+}
